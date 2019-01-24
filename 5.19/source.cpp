a6147e20182aa42fd4bf8b0913b24ef3ca602a66
morporkk <31141692+morporkk@users.noreply.github.com>
2019-01-24 02:13:18 +0100

    Update source.cpp

--- 5.19/source.cpp
+++ 5.19/source.cpp
@@ -8,13 +8,14 @@ int main() {
 
 
 	std::string rsp;
-	do {
+	do {	
+		std::cout<<"Input strings: "<<std::endl;
 		std::string s1, s2;
 		std::cin >> s1 >> s2;
 		std::cout << ((s1 == s2) ? "eual" : (s1 < s2) ? "s1<s2" : "s1>s2");
 		std::cout << std::endl;
 
-		std::cout << "Again?" << std::endl;
+		std::cout << "Again? (y/n)" << std::endl;
 		std::cin >> rsp;
 	} while (!rsp.empty() && tolower(rsp[0]) == 'y');
 }
