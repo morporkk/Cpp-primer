7a4856c16ef61a0c44e932e16e4f8f0003d4debe
morporkk <31141692+morporkk@users.noreply.github.com>
2019-01-28 19:50:06 +0100

    Create ref_param_addinfo.cpp

---
+++ Functions/ref_param_addinfo.cpp
@@ -0,0 +1,28 @@
+// using ref. to return additional info from function, we will find first occurnce of wanted char in string, and return it, but also using
+// reference we will change argument from calling funcion to indicate how many occurnces of that char happened
+
+#include <iostream>
+
+std::string::size_type find_char(const std::string &s, char c, std::string::size_type &occurs)
+{
+	auto ret = s.size();
+	occurs = 0;
+	for (decltype(ret) i = 0; i != s.size(); ++i)
+	{
+		if (s[i] == c) {
+			if (ret == s.size())
+				ret = i;
+			++occurs;
+		}
+	}
+	return ret;
+}
+
+
+int main() {
+   /* 
+   
+   *\
+	
+
+}
